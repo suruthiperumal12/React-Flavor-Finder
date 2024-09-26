0692db869e2d2d433575a88ac7845a1c1d03bae7
suruthiperumal12 <suruthi12jan@gmail.com>
2024-09-26 19:31:09 +0530

    modified files

--- src/components/Innercontainer.jsx
+++
@@ -1,5 +0,0 @@
-import { Children } from "react";
-import styles from "./innercontainer.module.css";
-export default function Innercontainer({ children }) {
-  return <div className={styles.innercontainer}>{children}</div>;
-}
